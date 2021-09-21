c211f958496798323ae73ac4c5caa325a36ce048
Vivian Vu <hlavu@myseneca.ca>
2021-09-21 00:26:08 -0400

    Add markdown support

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -1,6 +1,7 @@
 const { readFile, writeFile } = require("fs/promises");
 const path = require("path");
 const createHtml = require("create-html");
+var htmlBody = "";
 
 /**
  * Generates a new html file to a 'dist' directory.
@@ -8,51 +9,69 @@ const createHtml = require("create-html");
  * @param {string} stylesheetURL - A URL to a CSS stylesheet
  */
 async function generateHTMLFile(inputPath, stylesheetURL) {
-    try {
-        const filename = path.parse(inputPath).name;
-        const data = await readFile(inputPath, "utf-8");
+  try {
+    const filename = path.parse(inputPath).name;
+    const data = await readFile(inputPath, "utf-8");
 
-        // Retrieves HTML title (if any) - title is identified by being the first line of text followed by 2 blank lines.
-        const htmlTitle = data.match(/^.+(\r?\n\r?\n\r?\n)/);
-        // If there is an HTML title, wraps the title's content inside an <h1> tag.
-        const htmlBody =
-            htmlTitle == null
-                ? data
-                      .split(/\r?\n\r?\n/)
-                      .map((para) => {
-                          return `<p>${para.replace(/\r?\n/, " ")}</p>`;
-                      })
-                      .join(" ")
-                : `<h1>${htmlTitle[0].trim()}</h1> `.concat(
-                      data
-                          .substring(htmlTitle[0].length)
-                          .split(/\r?\n\r?\n/)
-                          .map((para) => {
-                              return `<p>${para.replace(/\r?\n/, " ")}</p>`;
-                          })
-                          .join(" ")
-                  );
+    // Retrieves HTML title (if any) - title is identified by being the first line of text followed by 2 blank lines.
+    const htmlTitle = data.match(/^.+(\r?\n\r?\n\r?\n)/);
+    // If there is an HTML title, wraps the title's content inside an <h1> tag.
 
-        const html = createHtml({
-            lang: "en",
-            head: `<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />`,
-            title: htmlTitle ? htmlTitle[0].trim() : filename,
-            css:
-                stylesheetURL != null
-                    ? stylesheetURL
-                    : "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css",
-            body: htmlBody,
-        });
-
-        await writeFile(
-            path.join(__dirname, "../", "dist", `${filename}.html`),
-            html
-        );
-        console.log(`${filename}.html has been created.`);
-    } catch (err) {
-        console.log(err);
+    if (path.extname(inputPath) === ".txt") {
+      // handles ".txt" content
+      htmlBody =
+        htmlTitle == null
+          ? data
+              .split(/\r?\n\r?\n/)
+              .map((para) => {
+                return `<p>${para.replace(/\r?\n/, " ")}</p>\n\n`;
+              })
+              .join(" ")
+          : `<h1>${htmlTitle[0].trim()}</h1>\n\n`.concat(
+              data
+                .substring(htmlTitle[0].length)
+                .split(/\r?\n\r?\n/)
+                .map((para) => {
+                  return `<p>${para.replace(/\r?\n/, " ")}</p>\n\n`;
+                })
+                .join(" ")
+            );
+    } else {
+      //  handles ".md" content
+      htmlBody = data
+        .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>") // replaces link -> <a href="$2">$1</a>
+        .split(/\r?\n\r?\n/)
+        .map(function (para) {
+          if (para.match(/(?<!#)#{1}\s/)) {
+            return `<h1>${para.replace(/# /, "")}</h1>\n\n`; // replaces # -> <h1></h1>
+          } else if (para.match(/(?<!#)#{2}\s/)) {
+            return `<h2>${para.replace(/## /, "")}</h2>\n\n`; // replace ## -> <h2></h2>
+          }
+          return `<p>${para.replace(/\r?\n/, " ")}</p>\n\n`;
+        })
+        .join(" ");
     }
+
+    const html = createHtml({
+      lang: "en",
+      head: `<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />`,
+      title: htmlTitle ? htmlTitle[0].trim() : filename,
+      css:
+        stylesheetURL != null
+          ? stylesheetURL
+          : "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css",
+      body: htmlBody,
+    });
+
+    await writeFile(
+      path.join(__dirname, "../", "dist", `${filename}.html`),
+      html
+    );
+    console.log(`${filename}.html has been created.`);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = generateHTMLFile;

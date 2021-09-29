b370a3d2849d1fd87f30cf2ba8655b4216ab276b
Minh Quan Nguyen <mqnguyen5@myseneca.ca>
2021-09-28 21:24:37 -0400

    Clean up text and markdown file converting logic

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -19,42 +19,40 @@ async function generateHTMLFile(inputPath, stylesheetURL, HTMLlanguage) {
         // If there is an HTML title, wraps the title's content inside an <h1> tag.
 
         if (path.extname(inputPath) === ".txt") {
-            // handles ".txt" content
+            // Handles ".txt" content
             htmlBody =
                 htmlTitle == null
                     ? data
-                          .split(/\r?\n\r?\n/)
+                          .split(/\r?\n\r?\n/g)
                           .map((para) => {
                               return `<p>${para.replace(/\r?\n/, " ")}</p>\n\n`;
                           })
-                          .join(" ")
+                          .join("\n")
                     : `<h1>${htmlTitle[0].trim()}</h1>\n\n`.concat(
                           data
                               .substring(htmlTitle[0].length)
-                              .split(/\r?\n\r?\n/)
+                              .split(/\r?\n\r?\n/g)
                               .map((para) => {
                                   return `<p>${para.replace(
                                       /\r?\n/,
                                       " "
                                   )}</p>\n\n`;
                               })
-                              .join(" ")
+                              .join("\n")
                       );
         } else {
-            // handles ".md" content
+            // Handles ".md" content
             htmlBody = data
                 .replace(/(?<!!)\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>") // replaces link -> <a href="URL">content</a>
                 .replace(/`(.*?)`/gim, "<code>$1</code>") // replaces single backtick-enclosed text -> <code>content</code>
-                .split(/\r?\n\r?\n/)
+                .split(/\r?\n\r?\n/g)
                 .map(function (para) {
-                    if (para.match(/(?<!#)#{1}\s/) != null) {
-                        return `<h1>${para.replace(/# /, "")}</h1>\n\n`; // replaces # -> <h1></h1>
-                    } else if (para.match(/(?<!#)#{2}\s/) != null) {
-                        return `<h2>${para.replace(/## /, "")}</h2>\n\n`; // replace ## -> <h2></h2>
-                    }
-                    return `<p>${para.replace(/\r?\n/, " ")}</p>\n\n`;
+                    return para
+                        .replace(/(^[^#](.*)$)/gim, "<p>$1</p>")
+                        .replace(/^##\s(.*$)/gim, "<h2>$1</h2>") // replaces ## Heading -> <h2>Heading</h2>
+                        .replace(/^#\s(.*$)/gim, "<h1>$1</h1>"); // replaces # Heading -> <h1>Heading</h1>
                 })
-                .join(" ");
+                .join("\n");
         }
 
         const html = createHtml({
